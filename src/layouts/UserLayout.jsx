86d8cd2759bf74c27e29756ff437bcb30118aea5
tapasvini1715 <adapt01@pfw.edu>
2025-10-14 12:30:54 -0400

    Add files via upload

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UserNavbar from "../components/UserNavbar";
+import Footer from "../components/Footer";
 
 export default function UserLayout({ children }) {
   return (
     <>
       <UserNavbar />
       <main className="mr-section container">{children}</main>
+      <Footer />
     </>
   );
 }
